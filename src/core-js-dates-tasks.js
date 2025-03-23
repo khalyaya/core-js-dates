9c523c7164cfae908fd447ab317b9d14f792daf4
Liza Khaletskaya <khaletskayali@gmail.com>
2025-03-23 09:53:24 +0300

    feat: solve 3 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -141,8 +141,14 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const time = new Date(date).getTime();
+  const arr = [];
+  Object.values(period).forEach((value) => {
+    const dateInMilliseconds = new Date(value).getTime();
+    arr.push(dateInMilliseconds);
+  });
+  return time >= arr[0] && time <= arr[1];
 }
 
 /**
@@ -156,8 +162,9 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const time = new Date(date);
+  return time.toLocaleString('en-US', { timeZone: 'Etc/UTC' });
 }
 
 /**
@@ -172,8 +179,17 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const daysInMonth = new Date(year, month, 0).getDate();
+  let counter = 0;
+  for (let i = 1; i <= daysInMonth; i += 1) {
+    const date = new Date(year, month - 1, i);
+    const day = date.getDay();
+    if (day === 0 || day === 6) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
 /**
